36d2e4fd3b3f3269ed1b31bfc320e72498739e67
Olena Patlata <elenapat468@gmail.com>
2022-11-03 14:07:40 +0200

    added async await

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,20 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
+  
 })
+module.exports={
+  devServer: {
+    proxy: {
+      '^/api': {
+        target: 'http://localhost:5000',
+        // target: 'http://127.0.0.1:3333',
+        // target: 'http://[::1]:8080',
+        logLevel: 'debug',
+        changeOrigin: true,
+        pathRewrite: {'^/api':'/'},
+        secure: false,
+      }
+    }
+  }
+}
